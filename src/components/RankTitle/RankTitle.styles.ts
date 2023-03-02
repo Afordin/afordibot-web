67ff328a48f22202226a3c209eb85be5a4a9eb38
SnowDrive <luc.moralesr@gmail.com>
2023-03-01 23:18:08 -0300

    fix: rename rank title folder

--- src/components/RankTitle/RankTitle.styles.ts
+++ src/components/RankTitle/RankTitle.styles.ts
@@ -8,9 +8,9 @@ export const RankTitleContainer = styled.div`
 export const RankTitle = styled.h2`
   background-clip: text;
   color: transparent;
+  font-family: 'Root Of Life';
+  font-size: clamp(3rem, 20vw, 7rem);
   transition: animation 0.3s ease-in-out;
   animation: bcMove 5s ease-in infinite;
-  font-family: 'RootOfLife';
-  font-size: clamp(3rem, 20vw, 7rem);
   /* line-height: 12rem; */
 `
