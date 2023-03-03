e1786b5dcf848be01946e64754e017f5452b2aea
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-03-03 12:57:05 -0600

    fix:add container for rankinglistitem, color palette adjust, quotation mark adjust

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,25 +1,24 @@
 import styled from 'styled-components'
 
 export const Header = styled.header`
-    display: grid;
-    place-items: center;
-    height: 50rem;
-    padding: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: transparent;
 `
 
 export const Title = styled.h1`
-    width: 100%;
-    background-image: linear-gradient(40deg, #fc00ff 0%, #00fffc 45%, #fffc00 100%);
-    -webkit-background-clip: text;
-    background-clip: text;
-    color: transparent;
-    font-size: clamp(2rem, 30vw, 34rem);
-    font-family: 'Root Of Life';
-    text-align: center;
-    transition: filter 0.3s ease-in-out;
-    user-select: none;
-    &:hover {
-        filter: drop-shadow(0 0 1rem #00bbbb);
-    }
+  width: 100%;
+  background-image: linear-gradient(40deg, #fc00ff 0%, #fc00ff 10%, #00fffc 50%, #fffc00 85%, #fffc00 100%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  font-size: clamp(2rem, 30vw, 34rem);
+  font-family: 'Root Of Life';
+  text-align: center;
+  transition: filter 0.3s ease-in-out;
+  user-select: none;
+  &:hover {
+      filter: drop-shadow(0 0 1rem #00bbbb);
+  }
 `
