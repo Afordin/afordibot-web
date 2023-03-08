9b1292e656a26fbf11b5841ad001b9887509638a
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-03-08 05:29:30 -0600

    feat: add rain component to home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Home } from './pages/Home'
 import { Fireflies } from 'components/Fireflies'
 import { FC, Fragment, ReactElement } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Rain } from 'components/Rain'
 
 export const App: FC = (): ReactElement => {
 	return (
 		<Fragment>
-			<Fireflies />
+			{Math.random() > 0.5 ? <Fireflies /> : <Rain />}
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
