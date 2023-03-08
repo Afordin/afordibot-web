a9b0165e0b5ffc4b077da1435cf64383f0180cf0
SnowDrive <luc.moralesr@gmail.com>
2023-03-08 10:51:50 -0300

    style: modify separator background color

--- src/styles/Separator.styles.ts
+++ src/styles/Separator.styles.ts
@@ -15,6 +15,6 @@ export const Separator = styled.div<SeparatorProps>`
 	height: ${(props) => separatorSizes[props.size || 'medium']};
 	margin-top: 10px;
 	margin-bottom: 26px;
-	background-color: #e0e0e0;
+	background-color: #999999;
 	border-radius: 4px;
 `
